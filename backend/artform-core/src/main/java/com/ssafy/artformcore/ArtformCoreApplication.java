cc9e730ec8b13de332df8f8e24ccd41e936835bf
김휘동 <rlagnlehd0417@gmail.com>
2025-03-16 23:20:57 +0900

    init: be core 서버 초기파일 추가

---
+++ backend/artform-core/src/main/java/com/ssafy/artformcore/ArtformCoreApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.artformcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtformCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtformCoreApplication.class, args);
+	}
+
+}
