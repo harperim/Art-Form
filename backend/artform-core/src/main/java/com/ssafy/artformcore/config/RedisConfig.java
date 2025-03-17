66a28d576be9d083c3784ee83bec8971f737221d
박준현 <wndrjrak12@gmail.com>
2025-03-18 07:31:49 +0900

    [BE] feat: REDIS 초기 설정

--- backend/artform-core/src/main/java/com/ssafy/artformcore/config/RedisConfig.java
+++ backend/artform-core/src/main/java/com/ssafy/artformcore/config/RedisConfig.java
@@ -6,7 +6,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 @RequiredArgsConstructor
@@ -23,6 +25,23 @@ public class RedisConfig {
     public LettuceConnectionFactory redisConnectionFactory() {
         // 싱글
         return new LettuceConnectionFactory(new RedisStandaloneConfiguration(host, port));
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        // Redis와 통신할 때 사용할 템플릿 설정
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+
+        // key, value에 대한 직렬화 방법 설정
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+
+        // hash key, hash value에 대한 직렬화 방법 설정
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new StringRedisSerializer());
+
+        return redisTemplate;
     }
 
 }
