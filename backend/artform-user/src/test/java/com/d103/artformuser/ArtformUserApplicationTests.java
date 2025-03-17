a5ff3f6a1f520667fdf8b347d5828403ad4c61af
김휘동 <rlagnlehd0417@gmail.com>
2025-03-17 17:28:51 +0900

    init: user서버 초기파일 추가

---
+++ backend/artform-user/src/test/java/com/d103/artformuser/ArtformUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.d103.artformuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtformUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
