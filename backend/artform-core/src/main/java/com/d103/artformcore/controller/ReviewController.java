22cc25810a95f991f8151bb09b3d70ac9993f831
박준현 <wndrjrak12@gmail.com>
2025-04-04 13:03:09 +0900

    [BE] feat: core swagger 전체 설정

--- backend/artform-core/src/main/java/com/d103/artformcore/controller/ReviewController.java
+++ backend/artform-core/src/main/java/com/d103/artformcore/controller/ReviewController.java
@@ -1,9 +1,10 @@
 package com.d103.artformcore.controller;
 
-import com.d103.artformcore.dto.PutReviewResponseDto;
 import com.d103.artformcore.dto.review.ReviewListDto;
 import com.d103.artformcore.dto.review.ReviewRequestDto;
 import com.d103.artformcore.service.ReviewService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +19,10 @@ public class ReviewController {
 
     private final ReviewService reviewService;
 
+    @Operation(summary = "해당 모델의 리뷰 조회",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "처리 성공!"),
+            })
     @GetMapping("/{modelId}")
     public ResponseEntity<ReviewListDto> getModelReviews(@PathVariable("modelId") Long modelId, Authentication authentication) {
         Long userId = Long.valueOf(authentication.getName());
@@ -25,6 +30,10 @@ public class ReviewController {
         return ResponseEntity.ok(reviews);
     }
 
+    @Operation(summary = "리뷰 등록",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "처리 성공!"),
+            })
     @PostMapping("/{modelId}")
     public ResponseEntity<ReviewListDto> createReview(@PathVariable("modelId") Long modelId, @RequestBody ReviewRequestDto reviewDto, Authentication authentication) {
 
@@ -33,6 +42,10 @@ public class ReviewController {
         return ResponseEntity.ok(responseDto);
     }
 
+    @Operation(summary = "리뷰 삭제",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "처리 성공!"),
+            })
     @DeleteMapping("/{reviewId}")
     public ResponseEntity<ReviewListDto> deleteReview(@PathVariable("reviewId") Long reviewId, Authentication authentication) {
         Long userId = Long.valueOf(authentication.getName());
