5ddc6380bfac7fd1c643fc8f0478a84fa3e38670
김휘동 <rlagnlehd0417@gmail.com>
2025-04-07 16:31:29 +0900

    fix: 비동기 처리 수정

--- backend/artform-core/src/main/java/com/d103/artformcore/config/AsyncConfig.java
+++ backend/artform-core/src/main/java/com/d103/artformcore/config/AsyncConfig.java
@@ -2,38 +2,27 @@ package com.d103.artformcore.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.AsyncTaskExecutor;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.task.DelegatingSecurityContextAsyncTaskExecutor;
 
 import java.util.concurrent.Executor;
 
 @Configuration
 @EnableAsync
 public class AsyncConfig {
+
     @Bean(name = "taskExecutor")
-    public Executor taskExecutor() {
+    public AsyncTaskExecutor taskExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(50);  // 기본 스레드 풀 크기
-        executor.setMaxPoolSize(1000); // 최대 스레드 풀 크기
-        executor.setQueueCapacity(500); // 대기 큐 크기
-        executor.setThreadNamePrefix("AsyncThread-");
-        executor.setWaitForTasksToCompleteOnShutdown(true);
-        executor.setAwaitTerminationSeconds(60);
-        // SecurityContext를 비동기 스레드에 전파하도록 설정
-        executor.setTaskDecorator(task -> {
-            SecurityContext context = SecurityContextHolder.getContext();
-            return () -> {
-                try {
-                    SecurityContextHolder.setContext(context);
-                    task.run();
-                } finally {
-                    SecurityContextHolder.clearContext();
-                }
-            };
-        });
+        executor.setCorePoolSize(3);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("async-thread-");
         executor.initialize();
-        return executor;
+        return new DelegatingSecurityContextAsyncTaskExecutor(executor);
     }
 }
